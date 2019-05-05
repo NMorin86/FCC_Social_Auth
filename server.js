f43165d88c6a96354a9f8c18e4b3a47313e5687a
Glitch (trite-termite) <none>
2019-05-05 16:26:16 +0000

    Import of freeCodeCamp/boilerplate-socialauth

--- server.js
+++ server.js
@@ -1,22 +1,95 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
-var express = require('express');
-var app = express();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const session     = require('express-session');
+const mongo       = require('mongodb').MongoClient;
+const passport    = require('passport');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+fccTesting(app); //For FCC testing purposes
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.set('view engine', 'pug')
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if(err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+      
+        app.use(session({
+          secret: process.env.SESSION_SECRET,
+          resave: true,
+          saveUninitialized: true,
+        }));
+        app.use(passport.initialize());
+        app.use(passport.session());
+      
+        function ensureAuthenticated(req, res, next) {
+          if (req.isAuthenticated()) {
+              return next();
+          }
+          res.redirect('/');
+        };
+
+        passport.serializeUser((user, done) => {
+          done(null, user.id);
+        });
+
+        passport.deserializeUser((id, done) => {
+            db.collection('socialusers').findOne(
+                {id: id},
+                (err, doc) => {
+                    done(null, doc);
+                }
+            );
+        });
+
+      
+        /*
+        *  ADD YOUR CODE BELOW
+        */
+      
+      
+      
+      
+      
+      
+      
+        /*
+        *  ADD YOUR CODE ABOVE
+        */
+      
+      
+        app.route('/')
+          .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/index');
+          });
+
+        app.route('/profile')
+          .get(ensureAuthenticated, (req, res) => {
+               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
+          });
+
+        app.route('/logout')
+          .get((req, res) => {
+              req.logout();
+              res.redirect('/');
+          });
+
+        app.use((req, res, next) => {
+          res.status(404)
+            .type('text')
+            .send('Not Found');
+        });
+      
+        app.listen(process.env.PORT || 3000, () => {
+          console.log("Listening on port " + process.env.PORT);
+        });  
+}});
